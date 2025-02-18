dd89028e0014f524cffa7dd1b63bdb068e6c9931
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-18 17:05:02 +0300

    Use viewsets for the subject endpoints instead of generic views

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,16 +1,17 @@
 from django.db.models import Count
-from rest_framework import generics
+from rest_framework import viewsets
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 
 
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count("courses"))
-    serializer_class = SubjectSerializer
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related("modules")
+    serializer_class = CourseSerializer
     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
