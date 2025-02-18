55a463e82bb52fc37c050e0770fd6aaeee45233d
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-18 17:13:39 +0300

    Use basic authentication to simulate the security of the Course Enroll api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from courses.api.pagination import StandardPagination
@@ -21,6 +22,8 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
