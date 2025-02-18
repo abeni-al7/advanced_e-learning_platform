b8e4759a04f3670f070f2ded7bd7fc12aadb94c3
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-18 17:46:56 +0300

    Create an API that enables enrolled users to access course content

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Course, Module, Subject
+from courses.models import Content, Course, Module, Subject
 
 
 class ModuleSerializer(serializers.ModelSerializer):
@@ -39,3 +39,41 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ["id", "title", "slug", "total_courses", "popular_courses"]
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ["order", "item"]
+
+
+class ModuleWithContentSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description", "contents"]
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
