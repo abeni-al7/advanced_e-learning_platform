e61f20fc2bdec9d4f028e05dfb0b22ac04a6e2a9
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-20 18:27:06 +0300

    Configure a hostname for prod

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -7,7 +7,7 @@ ADMINS = [
     ("Abenezer A. E", "abenezeralebachew3@gmail.com"),
 ]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["educaproject.com", "www.educaproject.com"]
 
 DATABASES = {
     "default": {
